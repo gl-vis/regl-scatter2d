947dbd30e6102c0dbbb2a55de766748d6955d746
dy <df.creative@gmail.com>
2019-03-08 22:03:31 -0500

    Fix missing points test

--- test/index.js
+++ test/index.js
@@ -123,29 +123,30 @@ t('missing points simple', async t => {
 
 	t.ok(eq(scatter, await load('./test/img/missing-points-simple.png'), {threshold: .1}))
 
+	regl.clear({color: [0,0,0,0]})
 	t.end()
 })
 
-t.skip('missing points #2334', t => {
+t('missing points #2334', async t => {
 	var scatter = createScatter(regl)
 
 	var data = []
 
-	for (let i = 0; i < 1e5; i++) {
-		data.push(+(new Date(i)), i)
+	for (let i = 0; i < 41111; i++) {
+		data.push(i, 0)
 	}
 
 	scatter.update([{
 		positions: data,
 		size: 3,
 		snap: true,
-		// range: [0, 0, 10, 100]
-		range: [0,0,100,100]
+		range: [0,-10,50,10]
 	}])
 	scatter.render()
 
-	document.body.appendChild(scatter.canvas)
+	t.ok(eq(scatter, await load('./test/img/missing-points-2334.png'), {threshold: .1}))
 
+	regl.clear({color: [0,0,0,0]})
 	t.end()
 })
 
